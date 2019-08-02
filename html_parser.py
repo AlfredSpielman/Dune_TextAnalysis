af8d91957f8833820118134c62ef97f28e7943ea
Bartosz Nowakowski <bartosz.alfred.nowakowski@gmail.com>
2019-08-02 13:14:02 +0200

    initialize html_parser.py

---
+++ html_parser.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib3
+import pandas as pd
+
+http = urllib3.PoolManager()
+
+url = 'https://raw.githubusercontent.com/BartekNowakowski/Dune_TextAnalysis/master/html/D1.html'
+response = http.request('GET', url)
+soup = BeautifulSoup(response.data, 'html.parser')
+
+title = soup.title.string
+
+paragraph = []
+for i in soup.find_all('p'):
+    paragraph.append(i)
+
+df_paragraph = pd.DataFrame(paragraph)
