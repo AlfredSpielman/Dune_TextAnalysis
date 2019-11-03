9b7dc40912d5c5bf113ff65fe7fbb92f5ccb70b0
Bartosz Nowakowski <bartosz.alfred.nowakowski@gmail.com>
2019-11-03 22:46:03 +0100

    small update

--- input_processing.py
+++ input_processing.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 
@@ -22,75 +21,82 @@ df_Library = df_Library[["path", "volume"]]
 DuneCronicles = []
 
 ChapterStarters = {
-    1:'blockquote',
-    2:'blockquote1a',
-    3:'extract',
-    4:'extract',
-    5:'epigraph',
-    6:'extracts',
-    7:'blockquote',
-    8:'blockquote'}
+    1: 'blockquote',
+    2: 'blockquote1a',
+    3: 'extract',
+    4: 'extract',
+    5: 'epigraph',
+    6: 'extracts',
+    7: 'blockquote',
+    8: 'blockquote'}
 
-for Book in range(1, 9): # Main loop over each book
+for Book in range(1, 9):  # Main loop over each book
     Dune = df_Library[df_Library["volume"] == str(Book)]["path"]
     Chapter = 0
 
-    for html_file in Dune: # Main loop over each chapter
+    for html_file in Dune:  # Main loop over each chapter
         with open(html_file, encoding="utf8") as markup:
             soup = BeautifulSoup(markup, 'html.parser')
 
             AllParagraphs = soup.body.find_all(['p', 'blockquote'])
             for row, paragraph in enumerate(AllParagraphs):
                 Class = paragraph.attrs['class'][0]
-                Text = paragraph.get_text().replace('\n        ', '').replace('\n', '').replace('  ', ' ').replace('  ', ' ') #?!
-                if Class == ChapterStarters[Book]: Chapter += 1
-                if Class == 'volume': Text = str.upper(Text)
+                Text = paragraph.get_text().replace(
+                    '\n        ', '').replace(
+                    '\n', '').replace(
+                    '  ', ' ').replace(
+                    '  ', ' ')
+                if Class == ChapterStarters[Book]:
+                    Chapter += 1
+                if Class == 'volume':
+                    Text = str.upper(Text)
 
                 DuneCronicles.append([Book, Chapter, Class, Text])
 
 # Adjust chapter numbers for 1st pages with volume names
 for row, value in enumerate(DuneCronicles):
-    if value[1] == 0: DuneCronicles[row][1] = 1
+    if value[1] == 0:
+        DuneCronicles[row][1] = 1
 
-df_DuneCronicles = pd.DataFrame(data=DuneCronicles, columns=['Book','Chapter','Class','Text'])
+df_DuneCronicles = pd.DataFrame(data=DuneCronicles, columns=['Book', 'Chapter', 'Class', 'Text'])
 
-Duplicates, EmptyLines  = [], []
-for row in range(0, len(DuneCronicles)): # range --> enumerate
+Duplicates, EmptyLines = [], []
+for row in range(0, len(DuneCronicles)):  # range --> enumerate
     # List to handle duplicates in Book 1 where 'blockquote' is a parent to 'noindent'
     # which causes duplicate record for chapter opening quotes
     if (DuneCronicles[row][0] == 1 or DuneCronicles[row][0] == 8) and \
-        DuneCronicles[row][2] == 'blockquote':
+            DuneCronicles[row][2] == 'blockquote':
         Duplicates.append(True)
     else:
         Duplicates.append(False)
-    # Empty lines at the begining of each chapter in Book 7 & 8
+    # Empty lines at the beginning of each chapter in Book 7 & 8
     if DuneCronicles[row][2] == 'linespace' or \
-       DuneCronicles[row][2] == 'right-para' or \
-       DuneCronicles[row][2] == 'center-para' or \
-       DuneCronicles[row][2] == 'linegroup' or \
-       DuneCronicles[row][2][:5] == 'image':
+            DuneCronicles[row][2] == 'right-para' or \
+            DuneCronicles[row][2] == 'center-para' or \
+            DuneCronicles[row][2] == 'linegroup' or \
+            DuneCronicles[row][2][:5] == 'image':
         EmptyLines.append(True)
     else:
         EmptyLines.append(False)
 
 Duplicates.insert(0, False)
-Duplicates = Duplicates[0:len(Duplicates)-1]
+Duplicates = Duplicates[0:len(Duplicates) - 1]
 
 df_DuneCronicles['Duplicates'] = Duplicates
 df_DuneCronicles['EmptyLines'] = EmptyLines
 
-df_DuneCronicles = df_DuneCronicles[df_DuneCronicles['Duplicates'] == False] # Remove duplicates from Book 1
-df_DuneCronicles = df_DuneCronicles[df_DuneCronicles['EmptyLines'] == False] # Remove empty lines from Book 7 & 8
+df_DuneCronicles = df_DuneCronicles[df_DuneCronicles['Duplicates'] == False]  # Remove duplicates from Book 1
+df_DuneCronicles = df_DuneCronicles[df_DuneCronicles['EmptyLines'] == False]  # Remove empty lines from Book 7 & 8
 df_DuneCronicles.reset_index(drop=True, inplace=True)
 
 Class_Identifiers = pd.read_excel(r'data\Class_Identifiers.xlsx')
 
 df_DuneCronicles = df_DuneCronicles.merge(Class_Identifiers,
-             how='left',
-             on=['Book','Class'],
-             sort=False)
+                                          how='left',
+                                          on=['Book', 'Class'],
+                                          sort=False)
 
-output_columns = ['Book','Chapter','Class','Identifier_A','Identifier_B','Text']
+output_columns = ['Book', 'Chapter', 'Class', 'Identifier_A', 'Identifier_B', 'Text']
 output_filename = r'output\DuneCronicles.csv'
 
 df_DuneCronicles = df_DuneCronicles[output_columns]
--- text_analysis.py
+++ text_analysis.py
@@ -1,17 +1,15 @@
 import pandas as pd
-import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from nltk import pos_tag, RegexpParser
 import string
-from num2words import num2words
 
 wl = WordNetLemmatizer()
 ps = PorterStemmer()
 
 # Original text
-DuneCronicles = pd.read_csv('output\DuneCronicles.csv')
+DuneCronicles = pd.read_csv(r'output\DuneCronicles.csv')
 
 # Converting all letters to lower case and drop empty lines
 DuneCronicles['Text'] = DuneCronicles['Text'].str.lower()
@@ -23,7 +21,7 @@ for row in DuneCronicles.itertuples():
     for sentence in sent_tokenize(row[6]):
         sentences.append((row[1], row[2], row[3], row[4], row[5], sentence))
 
-cols = ['Book','Chapter','Class','Identifier_A','Identifier_B','Text']    
+cols = ['Book', 'Chapter', 'Class', 'Identifier_A', 'Identifier_B', 'Text']
 newDuneCronicles = pd.DataFrame(sentences, columns=cols)
 
 # Remove punctuations & stopwords, tokenize sentences to words
@@ -33,12 +31,12 @@ stop_words = set(stopwords.words('english'))
 list_PorterStemmer, list_WordNetLemmatizer = [], []
 for row in newDuneCronicles.itertuples():
     stem_valid_words, lemma_valid_words = [], []
-    temp_word_tokenized = row[6].translate(translator)          # remove punctuations
-    temp_word_tokenized = word_tokenize(temp_word_tokenized)    # tokenize sentence into words
+    temp_word_tokenized = row[6].translate(translator)  # remove punctuations
+    temp_word_tokenized = word_tokenize(temp_word_tokenized)  # tokenize sentence into words
     for word in temp_word_tokenized:
-        if word not in stop_words:                              # remove stop words
-            stem = ps.stem(word)                                # PorterStemmer
-            lemma = wl.lemmatize(word)                          # WordNetLemmatizer
+        if word not in stop_words:  # remove stop words
+            stem = ps.stem(word)  # PorterStemmer
+            lemma = wl.lemmatize(word)  # WordNetLemmatizer
             stem_valid_words.append(stem)
             lemma_valid_words.append(lemma)
     list_PorterStemmer.append(stem_valid_words)
